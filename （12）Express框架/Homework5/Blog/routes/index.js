e7bf5074a2024fb1055a11795a9c3c1f5fcbb031
王亚红 <2976716563@qq.com>
2019-11-19 23:24:34 +0800

    .

---
+++ （12）Express框架/Homework5/Blog/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var {users,chapterList} = require('../data.json');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/',(req,res,next) => {
+  var username = req.body.username;
+  var pwd = req.body.pwd;
+  for(var i = 0;i<users.length;i++){
+    if(username == users[i].username && pwd == users[i].password){
+      res.render('list',{chapterList:chapterList})
+    }
+
+    else{
+      res.set('Content-Type','text/plain; charset=utf-8');    
+      res.end("用户名或密码错误！");
+    }
+  }
+})
+
+module.exports = router;
